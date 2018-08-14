4abba2e78726904c9fd9c237d9d03627f1d64cf0
guy <guyasulin13@gmail.com>
2018-08-14 18:01:38 +0300

    added welcome page

---
+++ js/script.js
@@ -0,0 +1,15 @@
+    $(document).ready(function(){
+
+        $("button").click(function(){
+            $(".html").animate({width:'90%'},2000);  
+        $(".css").animate({width:'90%'},2000);  
+        $(".js").animate({width:'70%'},2000);  
+        $(".jq").animate({width:'75%'},2000);  
+        $(".node").animate({width:'80%'},2000);  
+        $(".angular").animate({width:'90%'},2000);  
+        $(".mongo").animate({width:'90%'},2000);  
+        $(".express").animate({width:'70%'},2000);  
+ });
+      
+
+})
